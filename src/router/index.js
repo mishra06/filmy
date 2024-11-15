4c98097836703606197be2e475ec180d37fac018
mishra06 <mishra.vibhakar7262@gmail.com>
2024-11-16 00:36:49 +0530

    "updated"

--- src/router/index.js
+++ src/router/index.js
@@ -18,3 +18,29 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
+
+
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import App from "../App";
+// import Home from "../pages/Home";
+// import DetailsPage from "../pages/DetailsPage";
+// import SearchPage from "../pages/SearchPage";
+// import ExplorePage from "../pages/ExplorePage";
+
+// const AppRouter = () => {
+//     return (
+//         <Router>
+//             <App>
+//                 <Routes>
+//                     <Route path="/" element={<Home />} />
+//                     <Route path=":explore" element={<ExplorePage />} />
+//                     <Route path=":explore/:id" element={<DetailsPage />} />
+//                     <Route path="search" element={<SearchPage />} />
+//                 </Routes>
+//             </App>
+//         </Router>
+//     );
+// };
+
+// export default AppRouter;
